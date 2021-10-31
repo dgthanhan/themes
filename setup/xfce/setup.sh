a83e45705a9faaf6908fc5475a1587690c15b10e
Duong Thanh An <an.duong@evolus.vn>
2021-10-31 21:18:44 +0700

    Fix font/icon select

--- setup/xfce/setup.sh
+++ setup/xfce/setup.sh
@@ -14,12 +14,16 @@ tar -xvf ~/.fonts/Product-Sans-VH.tar.gz --directory ~/.fonts/ProductSans
 mkdir -p ~/.config/gtk-3.0/
 curl -0 -L https://github.com/dgthanhan/themes/raw/master/setup/xfce/files/gtk.css -o ~/.config/gtk-3.0/gtk.css
 
-curl -0 -L https://github.com/dgthanhan/themes/raw/master/setup/xfce/files/PanelProfile-convit.tar.bz2 ~/PanelProfile-convit.tar.bz2
+curl -0 -L https://github.com/dgthanhan/themes/raw/master/setup/xfce/files/PanelProfile-convit.tar.bz2 -o ~/PanelProfile-convit.tar.bz2
 xfce4-panel-profiles load ~/PanelProfile-convit.tar.bz2
 
 xfconf-query -c xsettings -p /Net/ThemeName -s "Arc-Lighter"
+xfconf-query -c xsettings -p /Net/IconThemeName -s "Paper"
 xfconf-query -c xfwm4 -p /general/theme -s Evolus-XHDPI
+xfconf-query -c xfwm4 -p /general/title_font -s "SVN-Product Sans Bold 12"
 xfconf-query -c xsettings -p /Gdk/WindowScalingFactor -s 2
+xfconf-query -c xsettings -p /Gtk/FontName -s "SVN-Product Sans 10"
+
 
 echo "export QT_SCALE_FACTOR=2" >> ~/.bashrc
 echo "export GDK_SCALE=2" >> ~/.bashrc
